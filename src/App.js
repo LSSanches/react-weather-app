8769ab105ceb7c2c32e30e8dc40f324397aa7cb2
LSSanches <101747287+LSSanches@users.noreply.github.com>
2022-10-26 11:14:22 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ export default function App() {
         <p>React Weather App</p>
         <Weather />
       </header>
+      <br />
+      <footer>
+        Coded by Lsanches, open-sourced at{" "}
+        <a
+          href="https://github.com/LSSanches/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
